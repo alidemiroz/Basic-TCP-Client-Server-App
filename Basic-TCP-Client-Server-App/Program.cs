82927aca90ff2aeb3952210bd43a4768e9a60c88
aliDemiroz <dmrzmehmetali@gmail.com>
2024-05-03 15:35:30 +0300

    Basic-TCP-Client-Server-Project

---
+++ Basic-TCP-Client-Server-App/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Basic_TCP_Client_Server_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 8000;
+            const string host = "localhost";
+
+            try
+            {
+                TcpListener server = new TcpListener(IPAddress.Parse(host), port);
+
+                server.Start();
+
+                Console.WriteLine($"Sunucu {host}:{port} portunda başlatıldı.");
+
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+
+                    NetworkStream stream = client.GetStream();
+
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                    string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                    Console.WriteLine($"Alınan mesaj: {message}");
+
+                    byte[] reply = Encoding.ASCII.GetBytes($"Sunucu cevabı: {message}");
+                    stream.Write(reply, 0, reply.Length);
+
+                    client.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Hata oluştu: {ex.Message}");
+            }
+            
+
+
+        }
+    }
+}
+
---
+++ Basic-TCP-Server-Client-App/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Basic_TCP_Server_Client_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 8000;
+            const string host = "localhost";
+
+            try
+            {
+                TcpClient client = new TcpClient(host,port);
+
+                Console.WriteLine($"Sunucuya ({host}:{port}) bağlanıldı.");
+
+                NetworkStream stream = client.GetStream();
+
+                string message = "Merhaba Sunucu.";
+
+                byte[] buffer = Encoding.ASCII.GetBytes(message);
+
+                stream.Write(buffer, 0, buffer.Length);
+
+                buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                string reply = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                Console.WriteLine($"Sunucu cevabı: {reply}");
+
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Hata oluştu: {ex.Message}");
+            }
+        }
+    }
+}
+
